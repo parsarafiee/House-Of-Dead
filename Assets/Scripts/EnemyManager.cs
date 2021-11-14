e8651b97a5b4f9ca6374de8b7d0f556430095db0
parsa <48556656+parsarafiee@users.noreply.github.com>
2021-11-13 23:51:21 -0500

    add zambi

--- Assets/Scripts/EnemyManager.cs
+++ Assets/Scripts/EnemyManager.cs
@@ -5,14 +5,18 @@ using UnityEngine.Animations;
 
 public class EnemyManager : MonoBehaviour
 {
+    public Transform player;
     public Animator animator;
     public float timeToWalk=0;
+    public float speed;
+    Rigidbody rb;
 
     float time;
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = this.gameObject.GetComponent<Rigidbody>();
         animator.SetInteger("Int", 0);
         //animator = gameObject.GetComponent<Animator>();
     }
@@ -20,12 +24,18 @@ public class EnemyManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         time+= Time.deltaTime;
         if (timeToWalk < time)
         {
             animator.SetInteger("Int", 1);
+            Vector3 pos = Vector3.MoveTowards(transform.position, player.position, speed*Time.deltaTime) ;
+            rb.MovePosition(pos);
+            transform.LookAt(player);
+         }
 
-        }
-        
+
+
+
     }
 }
